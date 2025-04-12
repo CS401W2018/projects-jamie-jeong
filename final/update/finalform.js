a19f35f5978ccc2eded6aa72dccf62d90e6b0ea2
jamie-jeong <jamiejihyejeong@gmail.com>
2025-04-12 02:56:54 -1000

    add

--- final/update/finalform.js
+++ final/update/finalform.js
@@ -35,13 +35,17 @@ document.getElementById("tripForm").addEventListener("submit", function (e) {
         comments: comments,           
     };
 
-    console.log(formData)
+    console.log(formData);
+
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "submit.json",true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             alert('Form submitted successfully.');
+
+            document.getElementById('myForm').reset();
         } else if (xhr.readyState === 4) {
             alert('Error submitting form.');
         }
